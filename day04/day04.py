e6d6d1bf4b895a869e2928c6d2770027febbeea2
radek <rstibora@gmail.com>
2018-12-05 22:06:21 +0100

    Add second star solution for day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,11 +1,10 @@
 import datetime
 import re
 
-def first_star(input):
+def collect_sleep_data(messages):
     def get_minutes(begin, end):
         return (end - begin).seconds // 60
 
-    messages = get_chronological(input)
     guard_sleep = {}
     current_guard_id = None
     for idx, message in enumerate(messages):
@@ -23,29 +22,17 @@ def first_star(input):
         elif action[:5] == "falls":
             number_of_minutes = get_minutes(messages[idx-1][0], timestamp)
             guard_sleep[current_guard_id][date_only] += number_of_minutes * ["awake"]
+    return guard_sleep
 
-    total_guard_sleep = {}
-    for guard_id, sleep_data in guard_sleep.items():
-        for data in sleep_data.values():
-            minutes_of_sleep = len([state for state in data if state == "asleep"])
-            if data[:-1] == "asleep":
-                minutes_of_sleep += 60 - len(data)
-            if not guard_id in total_guard_sleep:
-                total_guard_sleep[guard_id] = 0
-            total_guard_sleep[guard_id] += minutes_of_sleep
-
-    guard_id_with_most_sleep = sorted(list(total_guard_sleep.items()), key=(lambda x: x[1]), reverse=True)[0][0]
-    sleep_heatmap = 60 * [0]
-    for sleep_in_day in guard_sleep[guard_id_with_most_sleep].values():
-        for minute, state in enumerate(sleep_in_day):
-            if state == "asleep":
-                sleep_heatmap[minute] += 1
-    most_sleepy_minute = sleep_heatmap.index(max(sleep_heatmap))
-
-    return guard_id_with_most_sleep * most_sleepy_minute
-
-def second_star(input):
-    pass
+def generate_sleap_map(sleep_data):
+    sleep_heatmap = {}
+    for guard_id in sleep_data.keys():
+        sleep_heatmap[guard_id] = 60 * [0]
+        for sleep_in_day in sleep_data[guard_id].values():
+            for minute, state in enumerate(sleep_in_day):
+                if state == "asleep":
+                    sleep_heatmap[guard_id][minute] += 1
+    return sleep_heatmap
 
 def get_chronological(input):
     parsed_input = []
@@ -59,6 +46,36 @@ def get_chronological(input):
     parsed_input.sort(key=(lambda x: x[1]))
     parsed_input.sort(key=(lambda x: x[0]))
     return parsed_input
+
+def first_star(input):
+    sorted_messages = get_chronological(input)
+    guard_sleep = collect_sleep_data(sorted_messages)
+
+    total_guard_sleep = {}
+    for guard_id, sleep_data in guard_sleep.items():
+        for data in sleep_data.values():
+            minutes_of_sleep = len([state for state in data if state == "asleep"])
+            if data[:-1] == "asleep":
+                minutes_of_sleep += 60 - len(data)
+            if not guard_id in total_guard_sleep:
+                total_guard_sleep[guard_id] = 0
+            total_guard_sleep[guard_id] += minutes_of_sleep
+
+    guard_id_with_most_sleep = sorted(list(total_guard_sleep.items()), key=(lambda x: x[1]), reverse=True)[0][0]
+    sleep_heatmap = generate_sleap_map(guard_sleep)
+    most_sleepy_minute = sleep_heatmap[guard_id_with_most_sleep].index(max(sleep_heatmap[guard_id_with_most_sleep]))
+    return guard_id_with_most_sleep * most_sleepy_minute
+
+def second_star(input):
+    sorted_messages = get_chronological(input)
+    guard_sleep = collect_sleep_data(sorted_messages)
+    sleep_heatmap = generate_sleap_map(guard_sleep)
+
+    most_frequent_sleep = {}
+    for guard_id, sleap_map in sleep_heatmap.items():
+        most_frequent_sleep[guard_id] = max(sleap_map)
+    most_regular_sleeper = sorted(most_frequent_sleep.items(), key=lambda x: x[1], reverse=True)[0][0]
+    return most_regular_sleeper * sleep_heatmap[most_regular_sleeper].index(most_frequent_sleep[most_regular_sleeper])
 
 def day_04():
     with open("day04/input.txt", "r") as input_file:
