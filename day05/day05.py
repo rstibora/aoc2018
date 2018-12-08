d204885ba374576e6bdfc9f8ec114529c5a9c059
radek <rstibora@gmail.com>
2018-12-08 17:56:22 +0100

    Add first star solution for day 5

---
+++ day05/day05.py
@@ -0,0 +1,41 @@
+def first_star(input):
+    input = [char for char in input]
+    def valid_reduction_pair(a, b):
+        if a.lower() == b and a == b.upper():
+            return True
+        elif b.lower() == a and b == a.upper():
+            return True
+        else:
+            return False
+
+    def one_pass_reduce(input):
+        if len(input) == 1:
+            return input
+
+        for idx, _ in enumerate(input[:-1]):
+            if valid_reduction_pair(input[idx], input[idx + 1]):
+                input[idx] = "_"
+                input[idx+1] = "_"
+
+        return [poly for poly in input if poly != "_"]
+
+    current_length = None
+    while current_length != len(input):
+        current_length = len(input)
+        input = one_pass_reduce(input)
+
+    return len(input)
+
+def second_star(input):
+    pass
+
+def day_05():
+    with open("day05/input.txt", "r") as input_file:
+        input_data = input_file.readline()[:-1]
+        print("Day 5")
+        print("\tFirst star solution: {0}".format(first_star(input_data)))
+        print("\tSecond star solution: {0}".format(second_star(input_data)))
+        print()
+
+if __name__ == "__main__":
+    day_05()
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from day01 import day01
 from day02 import day02
 from day03 import day03
+from day04 import day04
+from day05 import day05
 
 if __name__ == "__main__":
     day01.day_01()
     day02.day_02()
     day03.day_03()
+    day04.day_04()
+    day05.day_05()
