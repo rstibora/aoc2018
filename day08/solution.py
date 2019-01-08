9f231ff3407aea629641fa5780d3b12327638523
radek <rstibora@gmail.com>
2019-01-08 21:44:55 +0100

    Refactor a bit

--- day08/solution.py
+++ day08/solution.py
@@ -3,18 +3,14 @@ def parse_input(input):
 
 def first_star(input):
     def sum_child_metadata(data_range):
-        meta_length = data_range[1]
-        header_length = 2
-        num_of_children = data_range[0]
-
         total_sum = 0
-        range_start = header_length
-        for _ in range(num_of_children):
-            child = sum_child_metadata(data_range[range_start:-meta_length])
+        range_start = 2
+        for _ in range(data_range[0]):
+            child = sum_child_metadata(data_range[range_start:-data_range[1]])
             range_start += child[0]
             total_sum += child[1]
-        total_sum += sum(data_range[range_start:(range_start+meta_length)])
-        return (range_start + meta_length, total_sum)
+        total_sum += sum(data_range[range_start:(range_start+data_range[1])])
+        return (range_start + data_range[1], total_sum)
 
     data = parse_input(input)
     return sum_child_metadata(data)[1]
