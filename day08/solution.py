08eaf9cf2e3cf69c8ebf79b0e5dc47dd3efda37f
radek <rstibora@gmail.com>
2019-01-09 18:28:06 +0100

    Add second star solution for day 8

--- day08/solution.py
+++ day08/solution.py
@@ -16,4 +16,25 @@ def first_star(input):
     return sum_child_metadata(data)[1]
 
 def second_star(input):
-    pass
+    def sum_child_metadata(data_range):
+        # Calculate values of children nodes.
+        range_start = 2
+        children = []
+        for _ in range(data_range[0]):
+            children.append(sum_child_metadata(data_range[range_start:-data_range[1]]))
+            range_start += children[-1][0]
+        # Caclulate value of this node.
+        total_sum = 0
+        if not len(children):
+            total_sum = sum(data_range[range_start:(range_start + data_range[1])])
+        else:
+            # To prevent off by one error since indices in meta data start from 1.
+            meta_indices = map(lambda x: x - 1, data_range[range_start:(range_start + data_range[1])])
+            for meta_idx in meta_indices:
+                try:
+                    total_sum += children[meta_idx][1]
+                except:
+                    pass
+        return (range_start + data_range[1], total_sum)
+    data = parse_input(input)
+    return sum_child_metadata(data)[1]
