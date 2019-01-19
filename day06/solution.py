27aa3d4b1e74a9a28dcac91181a9363c3c139187
radek <rstibora@gmail.com>
2019-01-19 15:20:02 +0100

    Add second star solution for day 6

--- day06/solution.py
+++ day06/solution.py
@@ -1,14 +1,28 @@
+import math
+
+SEARCH_DISTANCE = 10000
+
+def get_bbox(coords, inflate=0):
+    min_x = min([coord[0] for coord in coords])
+    max_x = max([coord[0] for coord in coords])
+    min_y = min([coord[1] for coord in coords])
+    max_y = max([coord[1] for coord in coords])
+    return ((min_x - inflate, min_y - inflate), (max_x + inflate, max_y + inflate))
+
+def get_ring_around_bbox(bbox, ring):
+    ring_points = set([(x, bbox[0][1] - ring) for x in range(bbox[0][0] - ring, bbox[1][0] + ring + 1)])
+    ring_points |= set([(x, bbox[1][1] + ring) for x in range(bbox[0][0] - ring, bbox[1][0] + ring + 1)])
+    ring_points |= set([(bbox[0][0] - ring, y) for y in range(bbox[0][1] - ring, bbox[1][1] + ring + 1)])
+    ring_points |= set([(bbox[1][0] - ring, y) for y in range(bbox[0][1] - ring, bbox[1][1] + ring + 1)])
+    return ring_points
+
+def manhattan_distance(coord_from, coord_to):
+    return abs(coord_to[0] - coord_from[0]) + abs(coord_to[1] - coord_from[1])
+
+def shift_coord(coord, shift):
+    return ((coord[0] + shift[0], coord[1] + shift[1]))
+
 def first_star(input):
-    def manhattan_distance(coord_from, coord_to):
-        return abs(coord_to[0] - coord_from[0]) + abs(coord_to[1] - coord_from[1])
-
-    def get_convex_hull(coords):
-        min_x = min([coord[0] for coord in coords])
-        max_x = max([coord[0] for coord in coords])
-        min_y = min([coord[1] for coord in coords])
-        max_y = max([coord[1] for coord in coords])
-        return ((min_x, min_y), (max_x, max_y))
-
     def find_closest_coord(coord, all_coords):
         distances = [(manhattan_distance(coord, other_coord), other_coord) for other_coord in all_coords]
         shortest_distance = min(distances, key=lambda x: x[0])
@@ -22,7 +36,7 @@ def first_star(input):
 
     coords = [(int(input_line.split(',')[0]), int(input_line.split(',')[1])) for input_line in input]
     coords_area = {key:0 for key in coords}
-    convex_hull = get_convex_hull(coords)
+    convex_hull = get_bbox(coords)
     for x in range(convex_hull[0][0], convex_hull[1][0] + 1):
         for y in range(convex_hull[0][1], convex_hull[1][1] + 1):
             closest = find_closest_coord((x, y), coords)
@@ -36,4 +50,38 @@ def first_star(input):
     return max([area for area in coords_area.values() if area])
 
 def second_star(input):
-    pass
+    def distance_to_all(coord, all_coords):
+        return sum([manhattan_distance(coord, coord_to) for coord_to in all_coords])
+
+    coords = [(int(input_line.split(',')[0]), int(input_line.split(',')[1])) for input_line in input]
+    area_points = set()
+    bbox = get_bbox(coords)
+    for x in range(bbox[0][0], bbox[1][0]):
+        for y in range(bbox[0][1], bbox[1][1]):
+            if distance_to_all((x, y), coords) < SEARCH_DISTANCE:
+                area_points.add((x, y))
+    # Check in increasingly larger ring around the bounding box.
+    new_points = True
+    ring = 1
+    while new_points:
+        old_point_count = len(area_points)
+        for point in get_ring_around_bbox(bbox, ring):
+            if distance_to_all(point, coords) < SEARCH_DISTANCE:
+                area_points.add(point)
+        ring += 1
+        new_points = len(area_points) != old_point_count
+    # Find the biggest area.
+    areas = []
+    point_stash = set([area_points.pop()])
+    while len(area_points):
+        area = 0
+        while len(point_stash):
+            point = point_stash.pop()
+            area += 1
+            neighbours = [shift_coord(point, (-1, 0)), shift_coord(point, (1, 0)), shift_coord(point, (0, -1)), shift_coord(point, (0, 1))]
+            for neighbour in neighbours:
+                if neighbour in area_points:
+                    area_points.remove(neighbour)
+                    point_stash.add(neighbour)
+        areas += [area]
+    return max(areas)
