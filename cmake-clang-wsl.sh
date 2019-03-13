e17618596c4efb4793c64d4215ab1321938533f6
Radek Stibora <rstibora@gmail.com>
2019-03-13 20:23:26 +0100

    Fix libc++ not being linked

--- cmake-clang-wsl.sh
+++ cmake-clang-wsl.sh
@@ -3,5 +3,6 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=clang-8 -DBOOST_INCLUDEDIR="/usr/include" -DBOOST_LIBRARYDIR="/usr/lib/x86_64-linux-gnu" ../day12
+# TODO: boost might be actually working without thiese hints.
+cmake -DCMAKE_CXX_COMPILER=clang-8 -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_SHARED_LINKER_FLAGS="-lc++ -lc++abi" ../day12
 cd -
